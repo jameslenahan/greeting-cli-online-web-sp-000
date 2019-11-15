9391419d7b4438db587ecc1aeb6c9daf6d7a58e2
James Lenahan <james.k.lenahan@gmail.com>
2019-11-15 17:41:06 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,12 @@
 # code the #greeting method here!
 def greeting(name)
+<<<<<<< HEAD
   puts "Hello Sally. It's nice to meet you."
 end
 
+=======
+  puts "Hello #{name}. It's nice to meet you."
+end
+
+name = ("Sally")
+>>>>>>> b9f3f61017aff4de21c37da3a41c1b102714b2fe
